bb1e97e4f9a0d8dc43d3e37095598da87e9e01ce
Fabricio Sena <fabricior.sena@outlook.com>
2020-05-31 02:53:48 -0300

    add header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { HeaderComponent } from './components/template/site/header/header.component';
+import { FooterComponent } from './components/template/site/footer/footer.component';
+import { NavComponent } from './components/template/site/nav/nav.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
