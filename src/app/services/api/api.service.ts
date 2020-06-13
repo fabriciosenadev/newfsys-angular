80568da3d7234f9dd30a8e6743bb9162760af50f
Fabricio Sena <fabricior.sena@outlook.com>
2020-06-13 18:54:19 -0300

    add api service

---
+++ src/app/services/api/api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  url:'http://localhost:3040';
+  
+  route:{
+    user:'user',
+    launching:'launching',
+  };
+
+  userAction:{
+    register:'register',
+    login:'login'
+  }
+  
+  constructor() { }
+}
