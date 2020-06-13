d765c62da051dd13841ad274b1f0acc3ac967a10
Fabricio Sena <fabricior.sena@outlook.com>
2020-06-13 18:54:54 -0300

    add service to register new users

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../../components/models/user.model';
+import { ApiService } from "../api/api.service";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  
+  constructor(
+    private http: HttpClient,
+    private api: ApiService
+    ) { }
 
-  constructor() { }
+    register(user: User): Observable<User>
+    {
+      const baseUrl = this.api.url;
+      const route = this.api.route.user;
+      const action = this.api.userAction.register;
+
+      console.log(`URL Target:${baseUrl}/${route}/${action}`);
+      return this.http.post<User>(`${baseUrl}/${route}/${action}`, user);
+    }
+  
+
 }
