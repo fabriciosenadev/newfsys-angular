4087269f9e2d322f4c4781368fdad57b5b0ccf22
Fabricio Sena <fabricior.sena@outlook.com>
2020-06-13 22:47:26 -0300

    add users properties to do the action

--- src/app/components/models/user.model.ts
+++ src/app/components/models/user.model.ts
@@ -1,5 +1,13 @@
 export interface User {
+    // dados enviados para API
     full_name: string,
     email: string,
     password: string,
+    
+    // usado apenas no front
+    verifyPass: string,
+
+    // retorno da API
+    success? : string,
+    token? : string,
 }
