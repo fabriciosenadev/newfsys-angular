c4a002fa46c0e7b93799828e10a2d92177f62ed2
Fabricio Sena <fabricior.sena@outlook.com>
2020-06-13 22:50:28 -0300

    add service to send user data to API

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -3,27 +3,57 @@ import { HttpClient } from '@angular/common/http';
 
 import { User } from '../../components/models/user.model';
 import { ApiService } from "../api/api.service";
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   
+
   constructor(
+    private snackBar: MatSnackBar,
     private http: HttpClient,
     private api: ApiService
     ) { }
 
+    // baseUrl = 'http://localhost:3030/';
+    baseUrl = this.api.url;
+    route = '';
+    userAction = '';
+
+    showMessage(
+      msg: string,
+      isError: boolean = false
+    ): void {
+      this.snackBar.open(msg, 'X', {
+        duration: 3000,
+        horizontalPosition: "right",
+        verticalPosition: "top",
+        panelClass: isError ? ['msg-error'] : ['msg-success']
+      })
+    }
+
     register(user: User): Observable<User>
     {
-      const baseUrl = this.api.url;
-      const route = this.api.route.user;
-      const action = this.api.userAction.register;
+      this.route = this.api.route.user;
+      this.userAction = this.api.userAction.register;
 
-      console.log(`URL Target:${baseUrl}/${route}/${action}`);
-      return this.http.post<User>(`${baseUrl}/${route}/${action}`, user);
+      return this.http.post<User>(
+        `${this.baseUrl}/${this.route}/${this.userAction}`, 
+        user
+      ).pipe( 
+        map(obj => obj),
+        catchError(error => this.errorHandler(error))
+      );
     }
   
-
+  // devolve um Observable vazio com mensagem de erro
+  errorHandler(error: any): Observable<any> {
+    console.log(error);
+    this.showMessage(error, true);
+    return EMPTY;
+  }
 }
