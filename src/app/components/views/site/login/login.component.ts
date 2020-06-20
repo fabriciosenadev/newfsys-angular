4660d2bba77f9ab12d6ad96a933738d4ab3eee83
Fabricio Sena <fabricior.sena@outlook.com>
2020-06-20 01:42:32 -0300

    add login method

--- src/app/components/views/site/login/login.component.ts
+++ src/app/components/views/site/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
 import { HeaderService } from 'src/app/services/template/header/header.service';
+import { UserLogin } from 'src/app/components/models/user.model';
+import { SessionService } from 'src/app/services/user/session.service';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +13,34 @@ import { HeaderService } from 'src/app/services/template/header/header.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private headerService: HeaderService) { 
+  user: UserLogin = {
+    email:'',
+    password:''
+  }
+
+  constructor(
+    private headerService: HeaderService,
+    private sessionService: SessionService,
+    private router: Router
+    ) { 
     headerService.headerData = {
       routeUrl: 'login'
     }
   }
 
   ngOnInit(): void {
+  }
+
+  login(): void
+  {
+    this.sessionService.login(this.user).subscribe(loginReturn => {
+      this.user = loginReturn      
+      if(loginReturn.token)
+      {
+        // console.log(loginReturn)
+        this.router.navigate(['/app/profile']);
+      }
+    });    
   }
 
 }
