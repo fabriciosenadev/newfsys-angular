55c3e26b28b6eb4692203b528fddc0955f469ee5
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-02 09:01:23 -0300

    add api system routes and fix formatting

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,27 +1,35 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
 
-  url = 'http://localhost:3040';
-  
-  route = {
-    user: 'user',
-    launching:'launching',
-  };
+    url = 'http://localhost:3040';
 
-  userAction = {
-    register:'register',
-    login:'login',
-    forgot: 'forgot',
-    resetPass: 'reset_password'
-  };
+    route = {
+        user: 'user',
+        launching: 'launching',
+        system: 'system',
+    };
+
+    userAction = {
+        register: 'register',
+        login: 'login',
+        forgot: 'forgot',
+        resetPass: 'reset_password'
+    };
+
+    systemRoute = {
+        user: 'user',
+        categories: 'categories',
+    };
 
     systemAction = {
-        userInfo: 'info'        
+        userInfo: 'info',
+        categoriesIn: 'in',
+        categoriesOut: 'out',
     };
-  
-  constructor() { }
+
+    constructor() { }
 }
