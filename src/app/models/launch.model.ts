d6c2121c68c80a64507c7a208dc6d1f263fcc2cb
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-03 11:28:42 -0300

    add launch model

---
+++ src/app/models/launch.model.ts
@@ -0,0 +1,16 @@
+export interface LaunchIn {
+    [x: string]: any,
+    date: Date,
+    id_category: Number,
+    description? : String,
+    value: Number,
+}
+
+export interface LaunchOut {
+    [x: string]: any,
+    date: Date,
+    id_category: Number,
+    description? : String,
+    value: Number,
+    id_pay_method: Number,
+}
