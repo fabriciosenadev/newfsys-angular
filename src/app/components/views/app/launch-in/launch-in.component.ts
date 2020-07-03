dc5745793207e1b264f256255f582825983a9f13
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-03 11:54:23 -0300

    add launch in component

---
+++ src/app/components/views/app/launch-in/launch-in.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Categories } from 'src/app/models/category.model';
+import { LaunchIn } from 'src/app/models/launch.model';
+
+import { SystemService } from 'src/app/services/system/system.service';
+import { LaunchService } from 'src/app/services/user/launch.service';
+
+
+@Component({
+    selector: 'app-launch-in',
+    templateUrl: 'launch-in.component.html',
+    styleUrls: ['./launch-in.component.css']
+})
+export class LaunchInComponent implements OnInit {
+
+    token: string = localStorage.getItem('authToken');
+
+    categories: Categories = {
+        id: 0,
+        categories: {},
+    };
+
+    launchIn: LaunchIn = {
+        date: new Date(),
+        id_category: 0,
+        description: '',
+        value: 0
+    }
+
+    constructor(
+        private systemService: SystemService,
+        private launchService: LaunchService
+    ) {
+        this.systemService.getCategories('in', this.token).subscribe(categoriesReturn => {
+            this.categories = categoriesReturn;
+        });
+    }
+
+    ngOnInit(): void {
+    }
+
+    newLaunchIn(): void {
+        this.launchService.storeIn(this.launchIn, this.token).subscribe(launchInReturn => {
+            this.launchIn = launchInReturn;
+            this.launchService.showMessage(launchInReturn.success);
+        });
+    }
+
+}
