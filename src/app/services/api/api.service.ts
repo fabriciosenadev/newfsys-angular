ccd319f7914b3dea46389846c69a8b5601195e4e
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-07 00:44:29 -0300

    fix api route

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-    url = 'https://newfsys-backend.herokuapp.com/';
+    url = 'https://newfsys-backend.herokuapp.com'; 
 
     route = {
         user: 'user',
