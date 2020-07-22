56f6910d600bc363580699e78dc4b45aaff9cedd
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-21 22:56:35 -0300

    add validators to login

--- src/app/components/views/app/login/login.component.ts
+++ src/app/components/views/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 
 import { HeaderService } from 'src/app/services/template/header/header.service';
 import { SessionService } from 'src/app/services/system/session.service';
@@ -18,10 +19,15 @@ export class LoginComponent implements OnInit {
         password: ''
     }
 
+    loginForm: FormGroup;
+
+    get lf() {return this.loginForm.controls; }
+
     constructor(
         private headerService: HeaderService,
         private sessionService: SessionService,
-        private router: Router
+        private router: Router,
+        private formBuilder: FormBuilder,
     ) {
         headerService.headerData = {
             routeUrl: 'login'
@@ -29,6 +35,28 @@ export class LoginComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            email: new FormControl(
+                this.user.email,
+                [
+                    Validators.email
+                ]
+            ),
+            password: new FormControl(
+                this.user.password,
+                [
+                    Validators.minLength(8)
+                ]
+            ),
+        });
+    }
+
+    onSubmit()
+    {
+        this.user.email = this.loginForm.value.email;
+        this.user.password = this.loginForm.value.password;
+
+        this.login();
     }
 
     login(): void {
@@ -36,7 +64,6 @@ export class LoginComponent implements OnInit {
             this.user = loginReturn
             if (loginReturn.token) {
                 localStorage.setItem('authToken', loginReturn.token);
-                // console.log(loginReturn)
                 this.sessionService.showMessage('Logado com sucesso');
                 this.router.navigate(['/app/profile']);
             }
