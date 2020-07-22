d77258367dc79cde279e9d2193a2b3559b7d6783
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-21 21:35:40 -0300

    add validators to all field of register screen

--- src/app/components/views/site/register/register.component.ts
+++ src/app/components/views/site/register/register.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+import { MustMatch } from 'src/app/_helpers/must-match.validator';
 
 import { HeaderService } from 'src/app/services/template/header/header.service';
 import { UserService } from 'src/app/services/user/user.service';
 
 import { UserRegister } from 'src/app/models/user/userRegister.model';
-import { FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
@@ -14,18 +16,28 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-    msg = [];
     user: UserRegister = {
         full_name: '',
         email: '',
         password: '',
         verifyPass: ''
     }
+    newUserForm: FormGroup;
+    submitted = false;
+
+    // get home input to receive data
+    get userEmail(): string {
+        return this.userService.userData.email;
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.newUserForm.controls; }
 
     constructor(
         private headerService: HeaderService,
         private userService: UserService,
-        private router: Router
+        private router: Router,
+        private formBuilder: FormBuilder,
     ) {
         headerService.headerData = {
             routeUrl: 'register'
@@ -34,32 +46,54 @@ export class RegisterComponent implements OnInit {
 
     ngOnInit(): void {
         this.user.email = this.userEmail;
+
+        this.newUserForm = this.formBuilder.group({
+            fullName: new FormControl(
+                this.user.full_name,
+                [
+                    Validators.required,
+                    Validators.minLength(10),
+                ]),
+            email: new FormControl(
+                this.user.email,
+                [
+                    Validators.required,
+                    Validators.email,
+                ]),
+            password: new FormControl(
+                this.user.password,
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                ]),
+            verifyPass: new FormControl(
+                this.user.verifyPass,
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                ]),
+        }, {
+            validator: MustMatch('password', 'verifyPass'),
+        });
     }
 
-    get userEmail(): string {
-        return this.userService.userData.email;
+    onSubmit() {
+        this.user.full_name = this.newUserForm.value.fullName;
+        this.user.email = this.newUserForm.value.email;
+        this.user.password = this.newUserForm.value.password;
+        this.user.verifyPass = this.newUserForm.value.verifyPass;
+
+        this.newUser();
     }
 
     newUser(): void {
-        // if (this.user.full_name.trim().length < 8)
-        // {
-        //   this.userService.showMessage('insert you full name', true);
-        // }
-        // else if(this.user.verifyPass != this.user.password )
-        // {
-        //   this.userService.showMessage('passwords doesn\'t match', true);
-        // }
-        // else 
-        // {
         this.userService.register(this.user).subscribe(userReturn => {
             this.user = userReturn
             if (userReturn.success) {
-                // this.msg = userReturn.success.split(' ');
                 this.userService.showMessage(userReturn.success);
                 this.router.navigate(['/login']);
             }
         });
-        // }
     }
 
 }
