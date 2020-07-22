a894ef9510b5ff695a4e016829cf1da72ed332af
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-21 22:25:53 -0300

    add validators to forgot and reset password

--- src/app/components/views/site/forgot/forgot.component.ts
+++ src/app/components/views/site/forgot/forgot.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, EMPTY } from 'rxjs';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 
 import { HeaderService } from 'src/app/services/template/header/header.service';
 import { UserService } from 'src/app/services/user/user.service';
+
 import { UserResetPass } from 'src/app/models/user/userResetPass.model';
+import { MustMatch } from 'src/app/_helpers/must-match.validator';
 
 @Component({
     selector: 'app-forgot',
@@ -20,10 +22,17 @@ export class ForgotComponent implements OnInit {
         userId: 0
     }
 
+    forgotForm: FormGroup;
+    resetForm: FormGroup;
+
+    get f() { return this.forgotForm.controls; }
+    get r() { return this.resetForm.controls; }
+
     constructor(
         private headerService: HeaderService,
         private userService: UserService,
-        private router: Router
+        private router: Router,
+        private formBuilder: FormBuilder,
     ) {
         headerService.headerData = {
             routeUrl: 'forgot'
@@ -31,6 +40,47 @@ export class ForgotComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.forgotForm = this.formBuilder.group({
+            email: new FormControl(
+                this.userResetPass.email,
+                [
+                    Validators.email
+                ],
+            )
+        });
+
+        this.resetForm = this.formBuilder.group({
+            password: new FormControl(
+                this.userResetPass.password,
+                [
+                    Validators.required,
+                    Validators.minLength(8)
+                ],
+            ),
+            verifyPass: new FormControl(
+                this.userResetPass.verifyPass,
+                [
+                    Validators.required,
+                    Validators.minLength(8)
+                ],
+            ),
+        },{
+            validator: MustMatch('password','verifyPass'),
+        });
+    }
+
+    onSubmit() {
+        if(this.userResetPass.userId == 0)
+        {
+            this.userResetPass.email = this.forgotForm.value.email;
+            this.forgotPass();
+        }
+        else
+        {
+            this.userResetPass.password = this.resetForm.value.password;
+            this.userResetPass.verifyPass = this.resetForm.value.verifyPass;
+            this.resetPass(this.userResetPass.userId.id);
+        }
     }
 
     forgotPass(): void {
