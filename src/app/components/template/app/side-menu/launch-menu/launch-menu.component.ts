d5fa5d485573aeed3cac2af03a306bdb64ee16b3
Fabricio Sena <fabricior.sena@outlook.com>
2020-07-28 08:47:08 -0300

    add launch menu to be used by side menu component

---
+++ src/app/components/template/app/side-menu/launch-menu/launch-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-launch-menu',
+  templateUrl: 'launch-menu.component.html',
+  styleUrls: ['./launch-menu.component.css']
+})
+export class LaunchMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
