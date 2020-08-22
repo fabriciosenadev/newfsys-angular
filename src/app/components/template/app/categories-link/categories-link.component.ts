8bc2e731613c48db98c710ab95f87707132987d2
Fabricio Sena <fabricior.sena@outlook.com>
2020-08-21 23:15:32 -0300

    [new][CategoriesLink]add a link to categories quick access

---
+++ src/app/components/template/app/categories-link/categories-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-categories-link',
+    templateUrl:'categories-link.component.html',
+    styleUrls: ['./categories-link.component.css']
+})
+export class CategoriesLinkComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
