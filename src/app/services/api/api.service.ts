fae906a017fb07692625c2c8638dca5e69b15f8e
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2020-10-05 23:39:17 -0300

    Test API

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-    url = 'https://newfsys-backend.herokuapp.com'; 
+    url = 'http://app-24628.nuvem-us-02.absamcloud.com/'; 
 
     route = {
         user: 'user',
